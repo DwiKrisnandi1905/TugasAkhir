baa7daf23efdf82f60a2d3306a203ce13562ff22
Maulana Arya Yoga J. <116637724+maulanaryoga@users.noreply.github.com>
2024-06-19 15:30:20 +0700

    update landing page

--- resources/views/Pelanggan/Page/Profile.blade.php
+++ resources/views/Pelanggan/Page/Profile.blade.php
@@ -1,8 +1,85 @@
 @extends('Pelanggan.Layout.index')
 
 @section('content')
-    <form action="{{ route('logout') }}" method="POST">
-        @csrf
-        <button type="submit" class="btn btn-danger">Logout</button>
-    </form>
+<div class="container rounded bg-white mt-5">
+    <div class="row">
+        <div class="col-md-4 border-right">
+            <div class="d-flex flex-column align-items-center text-center p-3 py-5">
+                <img class="rounded-circle mt-5" src="images/profil.png" width="90">
+                <span class="font-weight-bold">John Doe</span>
+                <span class="text-black-50">john_doe12@bbb.com</span>
+                <span>United States</span>
+            </div>
+        </div>
+        <div class="col-md-8">
+            <div class="p-3 py-5">
+                <div class="d-flex justify-content-between align-items-center mb-3">
+                    <h3 class="fw-bold">Profile Anda</h3>
+                </div>
+                <div class="row mt-2">
+                    <div class="col-md-6"><input class="form-control" placeholder="Nama Depan" value="John" readonly></div>
+                    <div class="col-md-6"><input class="form-control" value="Doe" placeholder="Nama Belakang" readonly></div>
+                </div>
+                <div class="row mt-3">
+                    <div class="col-md-6"><input class="form-control" placeholder="Email" value="john_doe12@bbb.com" readonly></div>
+                    <div class="col-md-6"><input class="form-control" value="" placeholder="No. Telepon" readonly></div>
+                </div>
+                <div class="row mt-3">
+                    <div class="col-md-6"><input class="form-control" placeholder="Alamat" value="" readonly></div>
+                    <div class="col-md-6"><input class="form-control" value="" placeholder="Negara" readonly></div>
+                </div>
+                <div class="row mt-3">
+                    <div class="col-md-6"><input class="form-control" placeholder="Bank yang Digunakan" value="" readonly></div>
+                    <div class="col-md-6"><input class="form-control" value="" placeholder="No. Akun" readonly></div>
+                </div>
+                <div class="mt-5 text-right">
+                    <button class="btn btn-danger" type="button" data-toggle="modal" data-target="#editProfileModal">Edit Profile</button>
+                </div>
+                <div class="mt-3 text-right">
+                    <form action="{{ route('logout') }}" method="POST">
+                        @csrf
+                        <button type="submit" class="btn btn-danger">Logout</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!-- Edit Profile Modal -->
+<div class="modal fade" id="editProfileModal" tabindex="-1" role="dialog" aria-labelledby="editProfileModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="editProfileModalLabel">Edit Profile</h5>
+            </div>
+            <div class="modal-body">
+                <div class="form-group">
+                    <label class="text-start fw-bold">First Name</label>
+                    <input type="text" class="form-control" id="modalFirstName" value="John">
+                </div>
+                <div class="form-group">
+                    <label class="text-start fw-bold">Last Name</label>
+                    <input type="text" class="form-control" id="modalLastName" value="Doe">
+                </div>
+                <div class="form-group">
+                    <label class="text-start fw-bold" >Email</label>
+                    <input type="email" class="form-control" id="modalEmail" value="john_doe12@bbb.com">
+                </div>
+                <div class="form-group">
+                    <label class="text-start fw-bold">Phone Number</label>
+                    <input type="text" class="form-control" id="modalPhone" value="+19685969668">
+                </div>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-danger">Save changes</button>
+            </div>
+        </div>
+    </div>
+</div>
 @endsection
+
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
--- resources/views/landingPage/landingPage.blade.php
+++ resources/views/landingPage/landingPage.blade.php
@@ -102,32 +102,50 @@
             background-color: #ff9a27;
         }
 
+        .card-body {
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            margin: 5%;
+        }
+
+
         #home {
-            background-image: url('images/bg3.jpg'); 
+            position: relative;
+            color: #fff;
+        }
+
+        #home .carousel-inner {
+            height: 100vh;
+        }
+
+        #home .carousel-item {
+            height: 100vh;
             background-repeat: no-repeat;
             background-position: center center;
             background-size: cover;
-            color: #fff;
             position: relative;
         }
 
-        #home::before {
-            content: '';
+        #home .carousel-item::before {
+            content: "";
             position: absolute;
             top: 0;
             left: 0;
             width: 100%;
             height: 100%;
-            background: rgba(0, 0, 0, 0.7);
+            background: rgba(0, 0, 0, 0.5); /* Dark overlay */
             z-index: 1;
         }
 
-        #home .container {
-            position: relative;
+        #home .carousel-caption {
             z-index: 2;
             background: rgba(0, 0, 0, 0.5);
-            padding: 50px;
+            padding: 10px;
             border-radius: 10px;
+            position: relative;
+            top: 40%;
+            left: 0%;
         }
 
         #products {
@@ -158,6 +176,12 @@
         #faq .accordion-item {
             margin-bottom: 10px;
         }
+
+        .card-img-top {
+            height: 200px; 
+            object-fit: cover;
+            padding: 2%;
+        }
     </style>
 </head>
 <body>
@@ -174,10 +198,10 @@
                         <a class="nav-link active" aria-current="page" href="#home" onclick="setActive(this)">Home</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#about" onclick="setActive(this)">Tentang Kami</a>
+                        <a class="nav-link" href="#products" onclick="setActive(this)">Produk</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#products" onclick="setActive(this)">Produk</a>
+                        <a class="nav-link" href="#about" onclick="setActive(this)">Tentang Kami</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="#contact" onclick="setActive(this)">Hubungi Kami</a>
@@ -196,43 +220,42 @@
     </nav>
 
     <!-- Home Section -->
-    <section id="home" class="py-5 text-center min-vh-100 d-flex-center">
-        <div class="container">
-            <h1 class="display-4">Selamat Datang di <span class="orange fw-bold">Alveen Clothing</span></h1>
-            <p class="lead fst-italic">"Wear Your Confidence"</p>
-            <a href="{{ route('login') }}" class="btn">Mulai Sekarang</a>
+    <section id="home" class="carousel slide" data-bs-ride="carousel">
+        <div class="carousel-inner">
+            <div class="carousel-item active" style="background-image: url('images/bg.jpg');">
+                <div class="carousel-caption d-flex-center">
+                    <div>
+                        <h1>Selamat Datang di <span class="orange fw-bold">Alveen Clothing</span></h1>
+                        <p class="lead fst-italic">"Wear Your Confidence"</p>
+                        <a href="{{ route('login') }}" class="btn">Mulai Sekarang</a>
+                    </div>
+                </div>
+            </div>
         </div>
     </section>
 
     <!-- Products Section -->
-    <section id="products" class="py-5 bg-light-custom min-vh-100">
-        <div class="container text-center">
-            <h2 class="mb-5 fw-bold">Produk Kami</h2>
+    <section id="products" class="py-5">
+        <div class="container">
+            <h2 class="text-center mb-5 fw-bold">Produk</h2>
             <div class="row">
-                <div class="col-md-4">
-                    <div class="card mb-4">
-                        <img src="https://via.placeholder.com/150" class="card-img-top" alt="Produk 1">
+                <!-- Product 1 -->
+                <div class="col-md-4 mb-4 mx-auto">
+                    <div class="card">
+                        <img src="images/1718171245_foto_produk_modal_0.jpg" class="card-img-top" alt="Product 1">
                         <div class="card-body">
-                            <h5 class="card-title">Produk 1</h5>
-                            <p class="card-text">Deskripsi singkat produk 1.</p>
+                            <h5 class="card-title text-center">Toko Baju</h5>
+                            <a href="{{ route('login') }}" class="btn">Lihat Sekarang</a>
                         </div>
                     </div>
                 </div>
-                <div class="col-md-4">
-                    <div class="card mb-4">
-                        <img src="https://via.placeholder.com/150" class="card-img-top" alt="Produk 2">
+                <!-- Product 2 -->
+                <div class="col-md-4 mb-4 mx-auto">
+                    <div class="card">
+                        <img src="images/1718171245_foto_produk_modal_0.jpg" class="card-img-top" alt="Product 2">
                         <div class="card-body">
-                            <h5 class="card-title">Produk 2</h5>
-                            <p class="card-text">Deskripsi singkat produk 2.</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-4">
-                    <div class="card mb-4">
-                        <img src="https://via.placeholder.com/150" class="card-img-top" alt="Produk 3">
-                        <div class="card-body">
-                            <h5 class="card-title">Produk 3</h5>
-                            <p class="card-text">Deskripsi singkat produk 3.</p>
+                            <h5 class="card-title text-center">Konveksi Baju</h5>
+                            <a href="{{ route('login') }}" class="btn">Lihat Sekarang</a>
                         </div>
                     </div>
                 </div>
@@ -240,128 +263,111 @@
         </div>
     </section>
 
+
     <!-- About Section -->
-    <section id="about" class="py-5 min-vh-100 d-flex-center bg-light-custom">
+    <section id="about" class="py-5">
         <div class="container">
-            <h2 class="text-center mb-5">Tentang Kami</h2>
+            <h2 class="text-center mb-5 fw-bold">Tentang Kami</h2>
             <div class="row">
-                <div class="col-md-6">
-                    <h3>Visi Kami</h3>
-                    <p>Menjadi pemimpin dalam industri kami dengan inovasi dan kualitas terbaik.</p>
-                    <h3>Misi Kami</h3>
-                    <ul>
-                        <li>Menyediakan produk dan layanan berkualitas tinggi kepada pelanggan.</li>
-                        <li>Membangun hubungan yang kuat dengan komunitas dan lingkungan.</li>
-                        <li>Mendorong inovasi dan kreativitas di setiap aspek bisnis kami.</li>
-                    </ul>
-                    <h3>Nilai-Nilai Kami</h3>
-                    <ul>
-                        <li>Integritas</li>
-                        <li>Komitmen</li>
-                        <li>Inovasi</li>
-                        <li>Kerjasama</li>
-                    </ul>
+                <div class="col-md-6 mb-4">
+                    <img src="images/about.jpg" class="img-fluid rounded" alt="Tentang Kami">
                 </div>
                 <div class="col-md-6">
-                    <h3>Sejarah Kami</h3>
-                    <p>Kami didirikan pada tahun 2000 dengan tujuan untuk memberikan solusi terbaik bagi pelanggan kami. Sejak saat itu, kami telah berkembang menjadi salah satu perusahaan terkemuka di industri kami, dengan tim yang terdiri dari para profesional yang berdedikasi.</p>
-                    <h3>Tim Kami</h3>
-                    <div class="row">
-                        <div class="col-sm-4 team-member">
-                            <img src="https://via.placeholder.com/150" class="img-fluid" alt="Team Member 1">
-                            <h5>John Doe</h5>
-                            <p>CEO</p>
-                        </div>
-                        <div class="col-sm-4 team-member">
-                            <img src="https://via.placeholder.com/150" class="img-fluid" alt="Team Member 2">
-                            <h5>Jane Smith</h5>
-                            <p>CTO</p>
-                        </div>
-                        <div class="col-sm-4 team-member">
-                            <img src="https://via.placeholder.com/150" class="img-fluid" alt="Team Member 3">
-                            <h5>Mike Johnson</h5>
-                            <p>CFO</p>
-                        </div>
-                    </div>
+                    <p class="text-justify">Alveen Clothing adalah sebuah toko pakaian dan penyedia jasa konveksi baju yang terletak di daerah Tembalang, sebuah kawasan yang dinamis di Kota Semarang. Kami tidak hanya menyediakan berbagai pilihan pakaian berkualitas tinggi, tetapi juga menawarkan layanan konveksi yang handal dan profesional. Lokasi kami yang strategis memudahkan pelanggan untuk mengakses toko kami, baik dari pusat kota maupun dari daerah sekitarnya. Di Alveen Clothing, kami berkomitmen untuk memberikan produk dan layanan terbaik, memastikan setiap pelanggan puas dengan pengalaman berbelanja dan hasil konveksi yang mereka terima</p>
+                    <p class="text-justify">Dengan fokus pada inovasi dan kualitas, Alveen Clothing terus berkembang dan beradaptasi dengan tren fashion terbaru. Kami bekerja sama dengan desainer berbakat dan menggunakan bahan-bahan pilihan untuk menciptakan produk yang tidak hanya nyaman dipakai, tetapi juga bergaya. Layanan konveksi kami mencakup berbagai kebutuhan, mulai dari seragam perusahaan, pakaian olahraga, hingga baju casual.</p>
                 </div>
             </div>
         </div>
     </section>
 
     <!-- Contact Section -->
-    <section id="contact" class="py-5 min-vh-100 d-flex-center">
+    <section id="contact" class="py-5 text-light">
         <div class="container">
-            <h2 class="text-center black">Hubungi Kami</h2>
+            <h2 class="text-center mb-5 fw-bold text-black">Hubungi Kami</h2>
             <form>
                 <div class="mb-3">
                     <label for="name" class="form-label">Nama</label>
-                    <input type="text" class="form-control" id="name" required>
+                    <input type="text" class="form-control" id="name" placeholder="Nama Anda">
                 </div>
                 <div class="mb-3">
                     <label for="email" class="form-label">Email</label>
-                    <input type="email" class="form-control" id="email" required>
+                    <input type="email" class="form-control" id="email" placeholder="name@example.com">
                 </div>
                 <div class="mb-3">
                     <label for="message" class="form-label">Pesan</label>
-                    <textarea class="form-control" id="message" rows="3" required></textarea>
+                    <textarea class="form-control" id="message" rows="3" placeholder="Tulis pesan Anda di sini"></textarea>
                 </div>
-                <button type="submit" class="btn">Kirim</button>
+                <button type="submit" class="btn">Kirim Pesan</button>
             </form>
         </div>
     </section>
 
-    <!-- Q&A -->
-    <section id="faq" class="py-5 bg-light-custom">
-        <div class="accordion accordion-flush bg-light-custom mx-5" id="accordionFlushExample">
-            <div class="accordion-item">
-                <h2 class="accordion-header" id="flush-headingOne">
-                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
-                    Dimana letak Alveen Clothing ?
-                    </button>
-                </h2>
-                <div id="flush-collapseOne" class="accordion-collapse collapse" aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushExample">
-                    <div class="accordion-body">Alveen Clothing terletak di Tembalang.</div>
+    <!-- FAQ Section -->
+    <section id="faq" class="py-5">
+        <div class="container">
+            <h2 class="text-center mb-5 fw-bold">Frequently Asked Question</h2>
+            <div class="accordion" id="faqAccordion">
+                <!-- FAQ Item 1 -->
+                <div class="accordion-item">
+                    <h2 class="accordion-header" id="faqHeadingOne">
+                        <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#faqCollapseOne" aria-expanded="true" aria-controls="faqCollapseOne">
+                            Dimana letak Alveen Clothing ?
+                        </button>
+                    </h2>
+                    <div id="faqCollapseOne" class="accordion-collapse collapse show" aria-labelledby="faqHeadingOne" data-bs-parent="#faqAccordion">
+                        <div class="accordion-body">
+                            Tembalang
+                        </div>
+                    </div>
                 </div>
-            </div>
-            <div class="accordion-item">
-                <h2 class="accordion-header" id="flush-headingTwo">
-                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseTwo" aria-expanded="false" aria-controls="flush-collapseTwo">
-                    Mengapa harus Alveen Clothing ?
-                    </button>
-                </h2>
-                <div id="flush-collapseTwo" class="accordion-collapse collapse" aria-labelledby="flush-headingTwo" data-bs-parent="#accordionFlushExample">
-                    <div class="accordion-body">Alveen Clothing menawarkan produk dengan kualitas tinggi yang terjamin. Bahan-bahan yang dipilih dengan cermat dan perhatian terhadap detail dalam desain membuat setiap produk menjadi nilai investasi yang baik bagi konsumen.</div>
+                <!-- FAQ Item 2 -->
+                <div class="accordion-item">
+                    <h2 class="accordion-header" id="faqHeadingTwo">
+                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#faqCollapseTwo" aria-expanded="false" aria-controls="faqCollapseTwo">
+                            Apa itu Alveen Clothing ?
+                        </button>
+                    </h2>
+                    <div id="faqCollapseTwo" class="accordion-collapse collapse" aria-labelledby="faqHeadingTwo" data-bs-parent="#faqAccordion">
+                        <div class="accordion-body">
+                            Toko dan Konveksi Baju
+                        </div>
+                    </div>
                 </div>
-            </div>
-            <div class="accordion-item">
-                <h2 class="accordion-header" id="flush-headingThree">
-                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseThree" aria-expanded="false" aria-controls="flush-collapseThree">
-                    No. Telepon yang bisa dihubungi ?
-                    </button>
-                </h2>
-                <div id="flush-collapseThree" class="accordion-collapse collapse" aria-labelledby="flush-headingThree" data-bs-parent="#accordionFlushExample">
-                    <div class="accordion-body">Jangan segan menghubungi kami pada +62-888-888-888.</div>
+                <!-- FAQ Item 3 -->
+                <div class="accordion-item">
+                    <h2 class="accordion-header" id="faqHeadingThree">
+                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#faqCollapseThree" aria-expanded="false" aria-controls="faqCollapseThree">
+                            No. yang bisa dihubungi ?
+                        </button>
+                    </h2>
+                    <div id="faqCollapseThree" class="accordion-collapse collapse" aria-labelledby="faqHeadingThree" data-bs-parent="#faqAccordion">
+                        <div class="accordion-body">
+                            +62 822 354 876
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
     </section>
 
     <!-- Footer -->
-    <footer class="py-4 footer-custom text-center">
-        <div class="container">
-            <p>&copy; 2024 Alveen Clothing. Semua hak dilindungi.</p>
+    <footer class="footer-custom py-4">
+        <div class="container text-center">
+            <p>&copy; 2024 Alveen Clothing. All rights reserved.</p>
         </div>
     </footer>
 
-    <!-- Bootstrap 5.3.0 JS Bundle CDN -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.0/js/bootstrap.bundle.min.js"></script>
-    <!-- Custom JS -->
     <script>
         function setActive(element) {
-            const links = document.querySelectorAll('.navbar-nav .nav-link');
-            links.forEach(link => link.classList.remove('active'));
+            var navLinks = document.querySelectorAll('.navbar-nav .nav-link');
+            navLinks.forEach(function(navLink) {
+                navLink.classList.remove('active');
+            });
             element.classList.add('active');
         }
+
+        var carousel = new bootstrap.Carousel('#home');
     </script>
 </body>
 </html>
